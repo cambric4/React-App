11b14ef8ffd52c25ddf430eb92cb37ce9bf6828c
Cambri Curtis <cambric4@gmail.com>
2025-06-13 09:42:18 -0600

    Used this to follow along with some assignments in the odin project

---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+    return <h1>&quot;Hello World! This is new and I dont know what I am doing yet haha. My favorite food is Pizza!&quot;</h1>;
+  }
+
+  export default Greeting;
